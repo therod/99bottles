ff698b98b555bab16c83a85f43c20925184dddf8
Rodrigo Haenggi <rodrigo@codegestalt.com>
2017-02-11 18:36:53 +0100

    add original failure

---
+++ lib/bottles.rb
@@ -0,0 +1,45 @@
+# I failed hard
+class Bottles
+  def verse(number)
+    <<-VERSE
+#{first_verse(number)}
+#{second_verse(number - 1)}
+  VERSE
+  end
+
+  def first_verse(number)
+    case number
+    when 1
+      "#{number} bottle of beer on the wall, #{number} bottle of beer."
+    when 0
+      "No more bottles of beer on the wall, no more bottles of beer."
+    else
+      "#{number} bottles of beer on the wall, #{number} bottles of beer."
+    end
+  end
+
+  def second_verse(number)
+    case number
+    when 1
+      "Take one down and pass it around, #{number} bottle of beer on the wall."
+    when 0
+      "Take it down and pass it around, no more bottles of beer on the wall."
+    when -1
+      "Go to the store and buy some more, 99 bottles of beer on the wall."
+    else
+      "Take one down and pass it around, #{number} bottles of beer on the wall."
+    end
+  end
+
+  def verses(one, two)
+    <<-VERSE
+#{first_verse(one)}
+#{second_verse(one - 1)}
+
+#{first_verse(two)}
+#{second_verse(two - 1)}
+  VERSE
+  end
+
+end
+
